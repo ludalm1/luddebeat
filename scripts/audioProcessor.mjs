a1da298a473ce2a206def2c09bbaf72f40e6ec54
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-08-26 13:47:04 +0800

    Update audioProcessor.mjs

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -293,7 +293,7 @@ class audioProcessor extends AudioWorkletProcessor {
 					this.getValuesVisualizer = (funcValue) => ((Math.sqrt(funcValue) * 32) & 255);
 					break;
 				case 'sinfmode':
-					this.getValues = (funcValue) => ((Math.sin(funcValue * Math.PI / 128 * 32)) / 127.5 - 1;
+					this.getValues = (funcValue) => ((Math.sin(funcValue * Math.PI / 128) * 32)) / 127.5 - 1;
 					this.getValuesVisualizer = (funcValue) => (((Math.sin(funcValue / (128 / Math.PI) + 0.01) * 128) & 255) + 127);
 					break;
 				case 'tanfmode':
