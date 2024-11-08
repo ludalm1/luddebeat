1331cb5ab2167594bf6a1f9776687d81e1b8ab97
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-08 22:44:07 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// register ServiceWorker, remember to use absolute path!
-// if (navigator.serviceWorker) {
-//   navigator.serviceWorker.register('/gfljs2100-user.github.io/gfljbeat2/sw.js', {scope: '/gfljs2100-user.github.io/gfljbeat2/'})
-// }
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/gfljbeat2/sw.js'
-}
