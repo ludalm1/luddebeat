6d491a0cf4844e2705e11e5bbd8eaedd6e1ffb65
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-08 22:57:50 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,15 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v3';
+const PRECACHE = 'precache-v2';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
-  'library',
-  'demo.js'
+  'index.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
