b0aec4617d0bf4af56081f31e7b4482a3d26a12d
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-09 00:34:49 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,3 @@
-// Check if service worker is supported
-if ("serviceWorker" in navigator) {
-  // Register the service worker
-  navigator.serviceWorker.register("service-worker.js")
-    .then(registration => {
-      console.log("Service worker registration succeeded:", registration);
-    })
-    .catch(error => {
-      console.error("Service worker registration failed:", error);
-    });
-} else {
-  console.error("Service workers are not supported.");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
 }
