38da62c664d054e4b306ca1a1734756670f5fa76
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-08 21:44:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// register ServiceWorker, remember to use absolute path!
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/andreykondakov.github.io/sw.js', {scope: '/andreykondakov.github.io/'})
+// }
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
