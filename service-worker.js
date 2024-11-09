1dff1787960071e6c241316d84e593414362cc80
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-09 12:13:49 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+  // register ServiceWorker, remember to use absolute path!
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/gfljbeat2/sw.js', {scope: '/{repository}/'})
+  };
