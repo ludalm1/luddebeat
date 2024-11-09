aabfdfc87858355d6798752066ad5a14af52919b
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-11-09 13:06:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var APP_PREFIX = 'gfljbeat2_'
-var VERSION = 'version_23'
+var APP_PREFIX = 'gfljbeat_'
+var VERSION = 'version_03'
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
   '/gfljbeat2/',
